60eb3a3ffa34cf692b2cc655bd161cece097bb71
nihat_ml <nihatrm-af206@code.edu.az>
2025-02-16 00:14:35 +0400

    KasAlm Group

---
+++ project/backend/server/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import cookieParser from "cookie-parser"
+import connectDB from "./config/mongodb.js"
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true}))
+
+app.listen(port, ()=> console.log(`Server started on PORT:${port}`))
