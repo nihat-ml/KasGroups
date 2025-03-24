23d81c536a444aa93f27f84d4bbbeb38e827615e
nihat_ml <nihatrm-af206@code.edu.az>
2025-03-25 00:51:50 +0400

    KasGroup

--- project/backend/server/controllers/authController.js
+++ project/backend/server/controllers/authController.js
@@ -71,8 +71,7 @@ export const login = async (req, res)=>{
         res.cookie('token', token ,{
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            none: "strict",
+            sameSite: 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
