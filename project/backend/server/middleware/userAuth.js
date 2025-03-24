9cf6b1a07de6f2e4e323c48c8e8ba08f79c5223a
huseynaghah <huseynaghanh@code.edu.az>
2025-03-24 14:51:35 +0400

    token fault check

--- project/backend/server/middleware/userAuth.js
+++ project/backend/server/middleware/userAuth.js
@@ -1,16 +1,18 @@
 import jwt from 'jsonwebtoken'
 
 const userAuth = async (req, res, next)=>{
-    const {token} = req.cookies
+    const {token} = req.cookies;
 
     if(!token){
         return res.json({success: false, message: "Not Authorized. Login Again"})
     }
 
+    console.log(token);
+
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
         if(tokenDecode.id){
-            req.body.userId = tokenDecode.id 
+            req.body.userId = tokenDecode.id;
         }else{
             return res.json({success: false, message: "Not Authorized. Login Again"})
         }
