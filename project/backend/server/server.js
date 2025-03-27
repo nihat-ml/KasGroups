04be3b189d335bbd0bd87d480e121b01660d4b8a
nihat_ml <nihatrm-af206@code.edu.az>
2025-03-27 22:39:28 +0400

    Domain

--- project/backend/server/server.js
+++ project/backend/server/server.js
@@ -43,7 +43,7 @@ app.set("trust proxy", 1);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.get('/api', (req, res) => res.send('Api Working'));
+app.get('/', (req, res) => res.send('Api Working'));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/products', productRouter);
