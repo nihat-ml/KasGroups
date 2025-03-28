2765278e4dfeba11117f67cf0d0dd85a770ad76e
nihat_ml <nihatrm-af206@code.edu.az>
2025-03-28 12:33:24 +0400

    KasLogo

--- project/frontend/src/assets/assets.js
+++ project/frontend/src/assets/assets.js
@@ -1,6 +1,6 @@
 import arrow_icon from './arrow_icon.svg'
 import lock_icon from './lock_icon.svg'
-import kaslogo1 from './kaslogo1.jpg'
+import kaslogo1 from './kaslogo1.png'
 import mail_icon from './mail_icon.svg'
 import person_icon from './person_icon.svg'
 import hand_wave from './hand_wave.png'
